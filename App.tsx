baa4453a8900e027b1fdf914aa4ded2dfad69e6a
murk-magnuson <markmagnusonprofessional@gmail.com>
2021-01-28 20:36:32 -0800

    Finalized Splash and Icon pngs.

--- App.tsx
+++ App.tsx
@@ -1,16 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { Image } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
 
 export default function App() {
+  const gifAsset = require('./assets/images/splash.gif')
   const isLoadingComplete = useCachedResources(); 
   const colorScheme = useColorScheme();
 
   if (!isLoadingComplete) {
     return null;
+    // if (true) { // Looks really trash sadly
+    // return (
+    //   <Image style={{backgroundColor: '#000'}} source={gifAsset}/>
+    // )
   } else {
     return (
       <SafeAreaProvider>
