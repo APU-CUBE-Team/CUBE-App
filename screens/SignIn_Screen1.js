f0468824610d885f2f0f66674d9468360ae390fd
murk-magnuson <markmagnusonprofessional@gmail.com>
2021-02-08 22:12:34 -0800

    Resolve merge conflict

--- screens/SignIn_Screen1.js
+++ screens/SignIn_Screen1.js
@@ -107,67 +107,6 @@ export default function SignInScreen(props) {
    }
 
    return (
-<<<<<<< HEAD
-   <KeyboardAvoidingView style={styles.container} behavior={(Platform.OS === 'ios') ? 'padding' : null}>
-      <SafeAreaView style={styles.container}>
-         
-
-         <StatusBar barStyle="light-content" />
-
-         <View style={styles.iconSafeArea}>
-            <Text style={styles.text3}>
-               CUBE
-            </Text>
-            <Image
-               style={styles.icon}
-               source={require('../assets/images/trans-icon.png')}
-            />
-         </View>
-         
-
-         <View style={styles.inputSafeArea}>
-            <TextInput
-               placeholder="Username"
-               value={username}
-               onChangeText={username => setUsername(username)}
-               style={styles.input}
-               autoCapitalize="none"
-               placeholderTextColor="#fff"
-
-
-
-            />
-            <TextInput
-               placeholder="Password"
-               value={password}
-               onChangeText={password => setPassword(password)}
-               secureTextEntry={true}
-               style={styles.input}
-               autoCapitalize="none"
-               placeholderTextColor="#fff"
-
-            />
-            
-            
-
-            <TouchableOpacity
-               style={styles.signInButton}
-               onPress={() => signInAuth(username, password)}>
-               <Text style={styles.text}>Sign In</Text>
-            </TouchableOpacity>
-
-            <TouchableOpacity onPress={() => alert("TODO")} >
-               <Text style={styles.text2}>Sign Up</Text>
-            </TouchableOpacity>
-            <TouchableOpacity onPress={() => alert("TODO")} >
-               <Text style={styles.text2}>Forgot Password</Text>
-            </TouchableOpacity>
-         </View>
-         
-        
-      </SafeAreaView> 
-   </KeyboardAvoidingView>
-=======
       <KeyboardAvoidingView style={styles.container} behavior={(Platform.OS === 'ios') ? 'padding' : null}>
          <SafeAreaView style={styles.container}>
 
@@ -213,9 +152,6 @@ export default function SignInScreen(props) {
                </TouchableOpacity>
 
                <TouchableOpacity onPress={() => alert("TODO")} >
-                  <Text style={styles.text2}>Sign Up</Text>
-               </TouchableOpacity>
-               <TouchableOpacity onPress={() => alert("TODO")} >
                   <Text style={styles.text2}>Forgot Password</Text>
                </TouchableOpacity>
             </View>
@@ -223,6 +159,5 @@ export default function SignInScreen(props) {
 
          </SafeAreaView>
       </KeyboardAvoidingView>
->>>>>>> origin/bugReportScreen
    );
 }
