7076ea977894e3fdecc871a67f2343e70566fda1
Justin-Watson <jwatson17@apu.edu>
2021-02-15 12:23:18 -0800

    Push please work

--- screens/CreateUser_Screen11.tsx
+++ screens/CreateUser_Screen11.tsx
@@ -112,7 +112,7 @@ const styles = StyleSheet.create({
 });
 
 
-export default function CreateUserScreen(props) {
+export default function CreateUserScreen() {
 
     const [firstName, setFirstName] = React.useState('');
     const [lastName, setLastName] = React.useState('');
