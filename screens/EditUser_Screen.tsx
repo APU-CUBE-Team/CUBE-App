3dac1a07e1798d1f8bf03c60ac944c244c7534d0
Justin-Watson <jwatson17@apu.edu>
2021-02-26 12:37:00 -0800

    For nate. Added Navigation and passing a user object to edit user role screen

--- screens/EditUser_Screen.tsx
+++ screens/EditUser_Screen.tsx
@@ -4,16 +4,17 @@ import { resetOrientation } from "../hooks/resetOrientation";
 import UserInfo from './UserInfo';
 
 export default function EditUserScreen({ navigation, route }) {
-  useFocusEffect(
-    React.useCallback(() => {
-      resetOrientation();
-    }, [])
-  );
-  
-  return (
-    <UserInfo 
-      create={false} 
-      user={null}
-    />
-  );
+    useFocusEffect(
+        React.useCallback(() => {
+            resetOrientation();
+        }, [])
+    );
+    console.log(route.params?.testUser)
+
+    return (
+        <UserInfo
+            create={false}
+            user={route.params?.testUser}
+        />
+    );
 }
