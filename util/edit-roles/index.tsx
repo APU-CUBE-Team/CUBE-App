003d72874167fcdeb0acd5f7ee66096b760e2437
Justin-Watson <jwatson17@apu.edu>
2021-02-28 21:55:54 -0800

    Worked with Josh to get backend returns to work... not much luck. need to learn async funcitons

--- util/edit-roles/index.tsx
+++ util/edit-roles/index.tsx
@@ -29,22 +29,58 @@ export function getUsersOfTeam() {
     });
 }
 
-export function getAdminsOfTeam() {
+// export async function getAdminsOfTeam() {
+//   let adminsArray: any[] = [];
+//   let response = await teamMembersDBColl
+//     .where("role", "==", "1")
+//     .get()
+//     .then((querySnapshot) => {
+//       // console.log("Admins:\n");
+//       const data1 = querySnapshot.docs;
+//       // console.log(data1);
+//       // data1.forEach(doc => {
+//       //   console.log(doc.data());
+//       // })
+//       data1.forEach((doc) => {
+//         // doc.data() is never undefined for query doc snapshots
+//         //console.log(doc.id, "=>", doc.data());
+//         adminsArray.push(doc.data());
+//       });
+
+//     });
+//     return Promise.resolve(adminsArray);
+// }
+
+export async function getAdminsOfTeam() {
   let adminsArray: any[] = [];
-  teamMembersDBColl
+  let response = await teamMembersDBColl
     .where("role", "==", "1")
     .get()
     .then((querySnapshot) => {
-      // console.log("Admins:\n");
-      querySnapshot.forEach((doc) => {
-        // doc.data() is never undefined for query doc snapshots
-        //console.log(doc.id, "=>", doc.data());
+      const data1 = querySnapshot.docs;
+
+      data1.forEach((doc) => {
         adminsArray.push(doc.data());
       });
-      return adminsArray;
     });
+  return Promise.resolve(adminsArray);
 }
 
 export function updateUser(data: any) {
   console.log("TODO");
 }
+
+// telemetryDBDoc.then(ret => {
+//   const data = ret.data();
+//   data.names.forEach((item: {item: string}) => {
+//       dataPoints.push({
+//           key: item,
+//           vals: data[item+"_Vals"],
+//           dates: data[item+"_Times"]
+//       })
+//   });
+//   setDataPoints(dataPoints);
+//   console.log("DataPoints: ", dataPoints)
+
+//   setFetched(true);
+// })
