d8c78ba0dc3a6bf9c47a56cbcce628de09223aaa
Nate <nathanielrbowman@gmail.com>
2021-03-21 15:31:50 -0700

    slight merge update

---
+++ util/recover-creds/index.tsx
@@ -0,0 +1,13 @@
+import { auth } from "../firebase-util";
+
+export async function sendPasswordResetEmail(email: string) {
+  let returnmessage: string;
+  /**
+  auth
+    .sendPasswordResetEmail(email)
+    .then(function () {})
+    .catch(function (error) {
+      returnmessage = error;
+    });
+ */
+}
