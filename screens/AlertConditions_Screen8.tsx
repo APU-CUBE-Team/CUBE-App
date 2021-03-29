984b1b18df53c060f61b5cb22ff2c2ef2e63b354
ColeG445 <cole.gunter08@gmail.com>
2021-03-29 16:39:46 -0700

    General buttons, needs testing

--- screens/AlertConditions_Screen8.tsx
+++ screens/AlertConditions_Screen8.tsx
@@ -1,31 +1,13 @@
 import * as React from "react";
-import { StyleSheet } from "react-native";
+import { StyleSheet, SafeAreaView } from "react-native";
 import { useFocusEffect } from "@react-navigation/native";
 import { resetOrientation } from "../hooks/resetOrientation";
 
 import EditScreenInfo from "../components/EditScreenInfo";
 import { Text, View } from "../components/Themed";
 import Colors from "../constants/Colors";
-
-// delete this comment after push 
-export default function AlertConditionsScreen() {
-  useFocusEffect(
-    React.useCallback(() => {
-      resetOrientation();
-    }, [])
-  );
-
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>AlertConditions</Text>
-      <View
-        style={styles.separator}
-        lightColor="#eee"
-        darkColor="rgba(255,255,255,0.1)"
-      />
-    </View>
-  );
-}
+import AppButton from "../components/Button";
+import FloatingLabelInput from "../components/floatingLabelInput";
 
 const styles = StyleSheet.create({
   container: {
@@ -44,3 +26,40 @@ const styles = StyleSheet.create({
     width: "80%",
   },
 });
+
+export default function AlertConditionsScreen() {
+  useFocusEffect(
+    React.useCallback(() => {
+      resetOrientation();
+    }, [])
+  );
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.title}>AlertConditions</Text>
+      <View
+        style={styles.separator}
+        lightColor="#eee"
+        darkColor="rgba(255,255,255,0.1)"
+      />
+      <AppButton
+      label="Parameter 1" // replace with data from db
+      onPressAction={() => alert("navigate to a pop-up")}
+      ></AppButton>
+      <AppButton
+      label="Parameter 2"
+      onPressAction={() => alert("navigate to a pop-up")}
+      ></AppButton>
+      <AppButton
+      label="Parameter 3"
+      onPressAction={() => alert("navigate to a pop-up")}
+      ></AppButton>
+      
+      <AppButton
+      label="New Parameter"
+      onPressAction={()=> alert("navigate to a new sub")}
+      ></AppButton>
+    </SafeAreaView>
+  );
+}
+
