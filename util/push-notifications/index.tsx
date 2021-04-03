b866880c874497fffe233d7cdbcd9834f5704c10
Nate <nathanielrbowman@gmail.com>
2021-04-03 16:46:28 -0700

    made getPushNotifications util/push-notifications

--- util/push-notifications/index.tsx
+++ util/push-notifications/index.tsx
@@ -8,15 +8,17 @@ export const alertsChanges = organizations
   });
 
 export async function pushNewAlertParameter(newParam: any) {
-  /**
-   * docID: {
-   *   telem: str,
-   *   msg: str, // sent to push notifications
-   *   op: char, // operator symbol/comparator
-   *   val: double // compared by the operator
-   *  }
-   */
-
+  // doc Example
+  {
+    /**
+     * docID: {
+     *   telem: str,
+     *   msg: str, // sent to push notifications
+     *   op: char, // operator symbol/comparator
+     *   val: double // compared by the operator
+     *  }
+     */
+  }
   organizations
     .doc("AdminOrganization")
     .collection("alerts")
@@ -26,5 +28,20 @@ export async function pushNewAlertParameter(newParam: any) {
     })
     .catch((e) => {
       console.log("An error occured: ", e);
+    });
+}
+
+export async function getPushNotifications() {
+  return organizations
+    .doc("AdminOrganization")
+    .collection("pushNotifications")
+    .get()
+    .then(function (querySnapshot) {
+      querySnapshot.forEach(function (doc) {
+        console.log(doc.id, " => ", doc.data());
+      });
+    })
+    .catch(function (error) {
+      console.log("Error getting documents: ", error);
     });
 }
