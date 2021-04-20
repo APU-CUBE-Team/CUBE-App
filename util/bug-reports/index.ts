3ce8170b42b58e86b4131fd88f189dd521502654
Nate <nathanielrbowman@gmail.com>
2021-04-20 16:16:54 -0700

    fixed weird merge conflict

--- util/bug-reports/index.ts
+++ util/bug-reports/index.ts
@@ -1,26 +1,15 @@
 import firebase from "firebase";
 import { auth, db } from "../firebase-util";
 
-// might need to initialize as admin? unless this works. we'll find out.
-
 // instantiate time stamp
 let currTime = firebase.firestore.Timestamp.fromDate(new Date());
 
-// get username from current logged in user
-
-let user = auth.currentUser;
-let userEmail = `${user?.email}`;
 // if collection exists, add to that collection. else, return error
 export async function reportBug(message: string) {
-<<<<<<< Updated upstream
-=======
-  let userEmail = `${auth.currentUser?.email}`;
   let userID = `${auth.currentUser?.uid}`;
->>>>>>> Stashed changes
+  let userEmail = `${auth.currentUser?.email}`;
   await db
-    .collection("TestOrganization") // TODO: designate collection to logged in organization
-    .doc("team")
-    .collection("bugReports")
+    .collection("BugReports") // TODO: designate collection to logged in organization
     .add({
       [userEmail]: {
         bugReports: {
